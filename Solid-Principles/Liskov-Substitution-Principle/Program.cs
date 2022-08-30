06086eb110a075989fef5a99ef425caf1654cebd
poyingHAHA <poying.code@gmail.com>
2022-08-31 07:52:36 +0800

    SOLID Principle - 3 Code violates Liskov Substitution Principle

---
+++ Solid-Principles/Liskov-Substitution-Principle/Program.cs
@@ -0,0 +1,40 @@
+﻿public class Rect
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+
+    public Rect()
+    {
+    }
+
+    public Rect(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+    public override string? ToString()
+    {
+        return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+    }
+}
+
+public class Square : Rect
+{
+    public new int Width { 
+        set { base.Width = base.Height = value; }
+    }
+    public new int Height
+    {
+        set { base.Width = base.Height = value; }
+    }
+}
+
+public class Demo
+{
+    static public int Area(Rect r) => r.Width * r.Height;    
+    static void Main(string[] args)
+    {
+        Rect rc = new Rect(2, 3);
+        Console.WriteLine($"{rc} has area {Area(rc)}");
+    }
+}
