7be5fe66a26024c678c9d7802441c0af2584024d
poyingHAHA <poying.code@gmail.com>
2022-08-30 22:24:12 +0800

    SOLID Principle - 2 Open-Closed Principle: Using Specification fix problem(the open closed principle states that parts of his system or the subsystems have to be opened for extension and should be closed for modifications.)

--- Solid-Principles/Open-Closed-Principle/Program.cs
+++ Solid-Principles/Open-Closed-Principle/Program.cs
@@ -51,6 +51,72 @@ public class ProductFilter
     }
 }
 
+public interface ISpecification<T>
+{
+    bool IsSatisfied(T t);
+}
+
+public interface IFilter<T>
+{
+    IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> spec);
+}
+
+public class ColorSpecification : ISpecification<Product>
+{
+    private Color color;
+
+    public ColorSpecification(Color color)
+    {
+        this.color = color;
+    }
+    public bool IsSatisfied(Product t)
+    {
+        return t.Color == color;
+    }
+}
+
+public class SizeSpecification : ISpecification<Product>
+{
+    private Size size;
+    public SizeSpecification(Size size)
+    {
+        this.size = size;
+    }
+
+    public bool IsSatisfied(Product t)
+    {
+        return t.Size == size;
+    }
+}
+
+public class AndSpecification<T> : ISpecification<T>
+{
+    private ISpecification<T> first, second;
+
+    public AndSpecification(ISpecification<T> first, ISpecification<T> second)
+    {
+        this.first = first ?? throw new ArgumentNullException(paramName: nameof(first));
+        this.second = second ?? throw new ArgumentNullException(paramName: nameof(second));
+    }
+
+    public bool IsSatisfied(T t)
+    {
+        return first.IsSatisfied(t) && second.IsSatisfied(t);
+    }
+}
+
+public class BetterFilter : IFilter<Product>
+{
+    public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> spec)
+    {
+        foreach(var item in items)
+        {
+            if(spec.IsSatisfied(item))
+                yield return item;
+        }
+    }
+}
+
 public class Demo
 {
     static void Main(string[] args)
@@ -61,11 +127,26 @@ public class Demo
 
         Product[] products = { apple, tree, house };
 
-        var pf = new ProductFilter();
-        Console.WriteLine("Green products (old): ");
-        foreach(var p in pf.FilterByColor(products, Color.Green))
+        //var pf = new ProductFilter();
+        //Console.WriteLine("Green products (old): ");
+        //foreach(var p in pf.FilterByColor(products, Color.Green))
+        //{
+        //    Console.WriteLine($" - {p.Name} is green");
+        //}
+
+        var bf = new BetterFilter();
+        Console.WriteLine("Green products (new): ");
+        foreach (var p in bf.Filter(products,  new ColorSpecification(Color.Green)))
         {
             Console.WriteLine($" - {p.Name} is green");
+        }
+
+        Console.WriteLine("Large blue items");
+        foreach(var p in bf.Filter(
+            products, 
+            new AndSpecification<Product>(new ColorSpecification(Color.Blue), new SizeSpecification(Size.Large))
+        )){
+            Console.WriteLine($" - {p.Name} is large and blue");
         }
     }
 }
