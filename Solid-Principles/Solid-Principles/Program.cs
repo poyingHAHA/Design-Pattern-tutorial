c0b9a9f4b42a5f8815f5dfcb8ed04b38fc3b744a
poyingHAHA <poying.code@gmail.com>
2022-08-30 21:15:20 +0800

    SOLID Principle - 1 Single Responsibility Principle(the whole point of the single responsibility principle is that a typical class is responsible for one thing and has one reason to change.)

---
+++ Solid-Principles/Solid-Principles/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using System.Diagnostics;
+
+public class Journal
+{
+    private readonly List<string> entries = new List<string>();
+    private static int count = 0;
+    
+    public int AddEntry(string text)
+    {
+        entries.Add($"{++count}: {text}");
+        return count;
+    }
+
+    public void RemoveEntry(int index)
+    {
+        entries.RemoveAt(index);
+    }
+
+    public override string ToString()
+    {
+        return string.Join(Environment.NewLine, entries);
+    }
+
+    // too many responsibilities
+    //public void Save(string filename){}
+    //public void Load(string filename){}
+}
+
+public class Persistence
+{
+    public void SaveToFile(Journal j, string filename, bool overwrite = false)
+    {
+        if(overwrite || File.Exists(filename)) File.WriteAllText(filename, j.ToString());
+    }
+}
+
+public class Demo
+{
+    static void Main(string[] args)
+    {
+        var j = new Journal();
+        j.AddEntry("I cried today");
+        j.AddEntry("I ate a bug");
+        Console.WriteLine(j);
+
+        var p = new Persistence();
+        var filename = @"E:/temp/journal.txt";
+        p.SaveToFile(j, filename, true);
+        Process.Start(filename);
+    }
+}
