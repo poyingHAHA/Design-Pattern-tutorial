bb64a24c4474c338700f060de2ae21bf15fe26a9
poyingHAHA <poying.code@gmail.com>
2022-08-31 21:33:56 +0800

    Builder - 3 Fluent Builder Inheritance without Recursive Generics

---
+++ Builder/Builder-Inheritance/Program.cs
@@ -0,0 +1,38 @@
+﻿
+public class Person
+{
+    public string Name;
+    public string Position;
+    public override string ToString()
+    {
+        return $"{nameof(Name)}: {Name}, {nameof(Position)}: {Position}";
+    }
+}
+
+public class PersonInfoBuilder
+{
+    protected Person person = new Person();
+    public PersonInfoBuilder called(string name)
+    {
+        person.Name = name;
+        return this;
+    }
+}
+
+public class PersonJobBuilder: PersonInfoBuilder
+{
+    public PersonJobBuilder WorkAsA(string position)
+    {
+        person.Position = position;
+        return this;
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = new PersonJobBuilder();
+        builder.called("Edwin").WorksAsA("Engineer"); 
+    }
+}
