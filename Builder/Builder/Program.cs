404ddd4cc58abc162ab35786bd8b3aabcee69d74
poyingHAHA <poying.code@gmail.com>
2022-08-31 15:17:17 +0800

    Builder - 2 with Builder

--- Builder/Builder/Program.cs
+++ Builder/Builder/Program.cs
@@ -1,5 +1,76 @@
 ﻿using System.Text;
 
+
+public class HtmlElement
+{
+    public string Name, Text;
+    public List<HtmlElement> Elements = new List<HtmlElement>();
+    private const int indentSize = 2;
+
+    public HtmlElement(){}
+
+    public HtmlElement(string name, string text)
+    {
+        Name = name ?? throw new ArgumentNullException(paramName: nameof(name));
+        Text = text ?? throw new ArgumentNullException(paramName: nameof(text));
+    }
+
+    public string ToStringImpl(int indent)
+    {
+        var sb = new StringBuilder();
+        var i = new string(' ', indentSize * indent);
+        sb.AppendLine($"{i}<{Name}>");
+
+        if(!string.IsNullOrWhiteSpace(Text))
+        {
+            sb.Append(new string(' ', indentSize * (indent + 1)));
+            sb.AppendLine(Text);
+        }
+
+        foreach (var e in Elements)
+        {
+            sb.Append(e.ToStringImpl(indent + 1));
+        }
+
+        sb.AppendLine($"{i}<{Name}>");
+        return sb.ToString();
+    }
+
+
+    public override string ToString()
+    {
+        return ToStringImpl(0);
+    }
+}
+
+public class HtmlBuilder
+{
+    private readonly string rootName;
+    HtmlElement root = new HtmlElement();
+
+    public HtmlBuilder(string rootName)
+    {
+        this.rootName = rootName;
+        root.Name = rootName;
+    }
+
+    public void AddChild(string childName, string childText)
+    {
+        var e = new HtmlElement(childName, childText);
+        root.Elements.Add(e);
+    }
+
+    public override string ToString()
+    {
+        return root.ToString();
+    }
+
+    public void Clear()
+    {
+        root = new HtmlElement { Name = rootName };
+    }
+}
+
 class Demo
 {
     static void Main(string[] args)
@@ -20,5 +91,11 @@ class Demo
         }
         sb.Append("</ul>");
         Console.WriteLine(sb);
+
+        // ================Builder Demo====================
+        var builder = new HtmlBuilder("ul");
+        builder.AddChild("li", "Hello");
+        builder.AddChild("li", "World");
+        Console.WriteLine(builder);
     }
 }
