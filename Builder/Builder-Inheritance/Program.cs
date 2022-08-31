64e3a0a89e405fe22e844cbbd4389847cef9441d
poyingHAHA <poying.code@gmail.com>
2022-08-31 22:01:34 +0800

    uilder - 3 Fluent Builder Inheritance with Recursive Generics

--- Builder/Builder-Inheritance/Program.cs
+++ Builder/Builder-Inheritance/Program.cs
@@ -3,28 +3,42 @@ public class Person
 {
     public string Name;
     public string Position;
+    public class Builder : PersonJobBuilder<Builder> { }
+    // Have a property called new, and every time you get a new, you try to construct a new person. It gives you a new builder.
+    public static Builder New => new Builder();  
     public override string ToString()
     {
         return $"{nameof(Name)}: {Name}, {nameof(Position)}: {Position}";
     }
 }
 
-public class PersonInfoBuilder
+public abstract class PersonBuilder
 {
     protected Person person = new Person();
-    public PersonInfoBuilder called(string name)
+
+    public Person Build()
     {
-        person.Name = name;
-        return this;
+        return person;
     }
 }
 
-public class PersonJobBuilder: PersonInfoBuilder
+public class PersonInfoBuilder<SELF> : PersonBuilder where SELF : PersonInfoBuilder<SELF>
 {
-    public PersonJobBuilder WorkAsA(string position)
+    public SELF called(string name)
+    {
+        person.Name = name;
+        return (SELF) this;
+    }
+}
+
+// So you can see that as you go ahead and you inherit builders from other builders.
+// It's going to be a big list of types or what's called a type list in languages such as C++.
+public class PersonJobBuilder<SELF>: PersonInfoBuilder<PersonJobBuilder<SELF>> where SELF : PersonJobBuilder<SELF>
+{
+    public SELF WorkAsA(string position)
     {
         person.Position = position;
-        return this;
+        return (SELF)this;
     }
 }
 
@@ -32,7 +46,7 @@ class Program
 {
     public static void Main(string[] args)
     {
-        var builder = new PersonJobBuilder();
-        builder.called("Edwin").WorksAsA("Engineer"); 
+        var me = Person.New.called("Edwin").WorkAsA("Engineer").Build();
+        Console.WriteLine(me);
     }
 }
