b323780244e0bd11e12b9321f5a5514a3b61d87b
poyingHAHA <poying.code@gmail.com>
2022-08-31 08:13:50 +0800

    SOLID Principle - 4 Code violates Interface Segregation Principle With too Large Interface

---
+++ Solid-Principles/Interface-Segregation-Principle/Program.cs
@@ -0,0 +1,55 @@
+﻿public class Document
+{
+
+}
+
+public interface IMachine
+{
+    void Print(Document d);
+    void Scan(Document d);
+    void Fax(Document d);
+}
+
+public class MultiFunctionPrinter : IMachine
+{
+    public void Fax(Document d)
+    {
+        //
+    }
+
+    public void Print(Document d)
+    {
+        //
+    }
+
+    public void Scan(Document d)
+    {
+        //
+    }
+}
+
+public class OldFashionedPrinter : IMachine
+{
+    public void Fax(Document d)
+    {
+        //
+    }
+
+    public void Print(Document d)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Scan(Document d)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Demo
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
