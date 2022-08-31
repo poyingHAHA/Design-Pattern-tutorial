a776c03c9445e749b55c17062473027d9950cd0b
poyingHAHA <poying.code@gmail.com>
2022-08-31 08:27:18 +0800

    SOLID Principle - 4 Interface Segregation Principle(A basic idea that if you have an interface which includes too much stuff, then just break it apart into smaller interfaces.)

--- Solid-Principles/Interface-Segregation-Principle/Program.cs
+++ Solid-Principles/Interface-Segregation-Principle/Program.cs
@@ -3,46 +3,56 @@
 
 }
 
-public interface IMachine
+public interface IPrinter
 {
     void Print(Document d);
+}
+
+public interface IScanner
+{
     void Scan(Document d);
-    void Fax(Document d);
 }
 
-public class MultiFunctionPrinter : IMachine
+public interface IMultiFunctionDevice : IScanner, IPrinter // ...
 {
-    public void Fax(Document d)
+
+}
+
+public class MultiFunctionMechine : IMultiFunctionDevice
+{
+    private IPrinter printer;
+    private IScanner scanner;
+    public MultiFunctionMechine(IPrinter printer, IScanner scanner)
     {
-        //
+        if(printer == null)
+        {
+            throw new ArgumentNullException(paramName: nameof(printer));
+        }
+        if(scanner == null)
+        {
+            throw new ArgumentNullException(paramName: nameof(scanner));
+        }
+
+        this.printer = printer;
+        this.scanner = scanner;
     }
 
     public void Print(Document d)
     {
-        //
+        printer.Print(d);
     }
-
     public void Scan(Document d)
     {
-        //
+        scanner.Scan(d);
     }
 }
 
-public class OldFashionedPrinter : IMachine
-{
-    public void Fax(Document d)
-    {
-        //
-    }
 
+public class OldFashionedPrinter : IPrinter
+{
     public void Print(Document d)
     {
-        throw new NotImplementedException();
-    }
-
-    public void Scan(Document d)
-    {
-        throw new NotImplementedException();
+        //
     }
 }
 
