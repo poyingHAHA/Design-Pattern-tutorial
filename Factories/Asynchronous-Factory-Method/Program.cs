79b32881dd80e905bfbff16938278ef8f55e2682
poyingHAHA <poying.code@gmail.com>
2022-09-01 13:13:30 +0800

    Factories - 2 Asynchronous Factory method

---
+++ Factories/Asynchronous-Factory-Method/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Foo
+{
+    private Foo()
+    {
+
+    }
+
+    private async Task <Foo> InitAsync()
+    {
+        await Task.Delay(1000);
+        return this;
+    }
+
+    public static Task<Foo> CreateAsync()
+    {
+        var result = new Foo();
+        return result.InitAsync();
+    }
+}
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        //var foo = new Foo();
+        //await foo.InitAsync();
+
+        Foo x = await Foo.CreateAsync();
+
+    }
+}
