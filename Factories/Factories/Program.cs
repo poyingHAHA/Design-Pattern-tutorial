e07f722bb69a661a41301f2436d3775e0262756f
poyingHAHA <poying.code@gmail.com>
2022-09-02 07:58:16 +0800

    Factories - 5 Inner Factory for private constructor

--- Factories/Factories/Program.cs
+++ Factories/Factories/Program.cs
@@ -1,35 +1,31 @@
-﻿// It's really up to you whether to make it static or not.
-public static class PointFactory
-{
-    public static Point NewCartesianPoint(double x, double y)
-    {
-        return new Point(x, y);
-    }
-    public static Point NewPolarPoint(double rho, double theta)
-    {
-        return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
-    }
-}
-
+﻿
 public class Point
 {
     private double x, y;
-    public Point(double x, double y)
+    private Point(double x, double y)
     {
         this.x = x;
         this.y = y;
     }
 
-    // factory method: The key point about this is that the name of the factory method is not directly tied to the name of the containing class.
-    //public static Point NewCartesianPoint(double x, double y)
-    //{
-    //    return new Point(x, y);
-    //}
+    // But if you do need to instantiate a new object, anytime somebody asks for something, then a property is a good alternative to a factory method.
+    public static Point Origin => new Point(0, 0);
+    public static Point Origin2 = new Point(0, 0); // better
 
-    //public static Point NewPolarPoint(double rho, double theta)
-    //{
-    //    return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
-    //}
+
+    // The only way point factory can access the private constructor is if point factory is, in fact an inner class of point.
+    // So now we have a private constructor for the point, but we can use this constructor because we are in our class of point.
+    public static class Factory
+    {
+        public static Point NewCartesianPoint(double x, double y)
+        {
+            return new Point(x, y);
+        }
+        public static Point NewPolarPoint(double rho, double theta)
+        {
+            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        }
+    }
 }
 
 public class Demo
@@ -37,7 +33,7 @@ public class Demo
     static void Main(string[] args)
     {
         //var p = new Point();
-        var point = PointFactory.NewPolarPoint(1.0, Math.PI / 2);
+        var point = Point.Factory.NewPolarPoint(1.0, Math.PI / 2);
         Console.WriteLine(point);
     }
 }
