3e01e02632fdb630788d5dd38fb15633b71c483c
poyingHAHA <poying.code@gmail.com>
2022-09-01 13:37:49 +0800

    Factories - 3 Factory: 當我們透過類別建立出實例的時候，其實感覺就像是一個工廠生產出了產品。而同一個工廠 (類別)，可以生產出無限多個同樣者產品 (實例)。

--- Factories/Factories/Program.cs
+++ Factories/Factories/Program.cs
@@ -1,29 +1,43 @@
-﻿public class Point
+﻿// It's really up to you whether to make it static or not.
+public static class PointFactory
 {
-    private double x, y;
-    private Point(double x, double y)
-    {
-        this.x = x;
-        this.y = y;
-    }
-
-    // factory method: The key point about this is that the name of the factory method is not directly tied to the name of the containing class.
     public static Point NewCartesianPoint(double x, double y)
     {
         return new Point(x, y);
     }
-
     public static Point NewPolarPoint(double rho, double theta)
     {
         return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
     }
 }
 
+public class Point
+{
+    private double x, y;
+    public Point(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    // factory method: The key point about this is that the name of the factory method is not directly tied to the name of the containing class.
+    //public static Point NewCartesianPoint(double x, double y)
+    //{
+    //    return new Point(x, y);
+    //}
+
+    //public static Point NewPolarPoint(double rho, double theta)
+    //{
+    //    return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+    //}
+}
+
 public class Demo
 {
     static void Main(string[] args)
     {
-        var point = Point.NewPolarPoint(1.0, Math.PI/2);
+        var p = new Point();
+        var point = PointFactory.NewPolarPoint(1.0, Math.PI / 2);
         Console.WriteLine(point);
     }
 }
